675a9f4bac9f79442213290c59a397a457b6bc08
Boris Stukov <stukov.boris@gmail.com>
2020-05-22 10:47:58 +0600

    lesson03 ex_base_01

---
+++ lesson03/tests/sum.test.js
@@ -0,0 +1,22 @@
+describe('sumOfPositive', () => {
+    it('empty value', () => {
+        const result = sumOfPositive();
+        assert.equal(result, 'Value is empty');
+    });
+    it('value is not array', () => {
+        const result = sumOfPositive("asd");
+        assert.equal(result, 'Value is not array');
+    });
+    it('empty array[]', () => {
+        const result = sumOfPositive([]);
+        assert.deepEqual(result, {count: 0, sum: 0});
+    });
+    it('no Positive number', () => {
+        const result = sumOfPositive([-1, -2, -7, -6]);
+        assert.deepEqual(result, {count: 0, sum: 0});
+    });
+    it('incorrect data in array', () => {
+        const result = sumOfPositive([3, -2, "asd", 6]);
+        assert.equal(result, 'Invalid array');
+    });
+});
