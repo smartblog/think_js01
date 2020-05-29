b9184f8cfcf2f8fef2d7ed82e2c3f7fc6704e1bf
Boris Stukov <stukov.boris@gmail.com>
2020-05-29 18:29:47 +0600

    fcs view buy-ticket

--- lesson05/fcs/buy-ticket.form.js
+++ lesson05/fcs/buy-ticket.form.js
@@ -2,6 +2,7 @@ const formBuyTicket = document.getElementById('buy-ticket-form');
 
 formBuyTicket.addEventListener('submit', submitBuyTicket);
 
+
 /**
  * Обработчик отправки формы
  * @param {KeyboardEvent} event
@@ -12,11 +13,13 @@ function submitBuyTicket(event) {
 
     const formData = {
         flightName: formBuyTicket.elements.flight.value,
-        fullName: formBuyTicket.elements.fullname.value
+        fullName: formBuyTicket.elements.fullname.value,
+        seatType: parseInt(formBuyTicket.elements.seatType.value)
     };
 
     try {
-        buyTicket(world.flights, formData.flightName, Date.now(), formData.fullName, type = 0);
+        const ticket = buyTicket(world.flights, formData.flightName, Date.now(), formData.fullName, type = formData.seatType);
+        createDivTicket(ticket);
         alert('You successfully buy ticket');
     } catch (error) {
         console.error(error);
@@ -26,4 +29,28 @@ function submitBuyTicket(event) {
     updateView();
 
     formBuyTicket.reset();
+}
+
+function createDivTicket(ticket) {
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const ul = document.createElement("ul");
+
+    h2.textContent = "Ticket " + ticket.id
+    div.append(h2);
+
+    const li1 = document.createElement("li");
+    li1.textContent = "Seat: " + ticket.seat
+    ul.append(li1);
+
+    const li2 = document.createElement("li");
+    li2.textContent = "Type: " + ticket.type
+    ul.append(li2);
+
+    const li3 = document.createElement("li");
+    li3.textContent = "FullName: " + ticket.fullName
+    ul.append(li3);
+
+    div.append(ul);
+    document.body.append(div)
 }
