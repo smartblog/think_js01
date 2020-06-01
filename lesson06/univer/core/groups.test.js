72243579e6f43b9084d979f4014dc4cd0131cb59
Boris Stukov <stukov.boris@gmail.com>
2020-06-01 20:53:26 +0600

    refactoring tests

--- lesson06/univer/core/groups.test.js
+++ lesson06/univer/core/groups.test.js
@@ -1,9 +1,11 @@
 
-describe('Create Group', () => {
-    it('Empty number of group', () => {
-        expect(() => new Group()).to.throw('You need enter number of group');
-    });
-    it('Value number is not number', () => {
-        expect(() => new Group([1,2,3])).to.throw('Value number is not number');
+describe('class Group', () => {
+    describe('create', () => {
+        it('Empty number of group', () => {
+            expect(() => new Group()).to.throw('You need enter number of group');
+        });
+        it('Value number is not number', () => {
+            expect(() => new Group([1,2,3])).to.throw('Value number is not number');
+        });
     });
 });
